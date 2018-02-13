b7d6ed8b656fce671b914e363daab5860708d509
吉永杰 <J@yoshinagamasarus-MacBook-Pro.local>
2018-02-12 22:07:06 -0800

    Solution to 378

---
+++ KthSmallestElementInASortedMatrix/Solution.java
@@ -0,0 +1,35 @@
+//Priority Queue to build min heap
+
+class Solution {
+	public int kthSmallest(int[][] matrix, int k) {
+		int n = matrix.length;
+
+		PriorityQueue<Tuple> pq = new PriorityQueue();
+
+		for(int i = 0; i < n; i++) {
+			pq.offer(new Tuple(0, i, matrix[0][i]));
+		}
+
+		for(int i = 0; i < k-1; i++) {
+			Tuple t = pq.poll();
+			if(t.x == n-1) continue;
+			pq.offer(new Tuple(t.x+1, t.y, matrix[t.x+1][t.y]));
+		}
+		return pq.poll().val;
+	}
+}
+
+class Tuple implements Comparable<Tuple> {
+	int x, y, val;
+
+	public Tuple(int x, int y, int val) {
+		this.x = x;
+		this.y = y;
+		this.val = val;
+	}
+
+	@Override
+	public int compareTo(Tuple t) {
+		return this.val - t.val;
+	}
+}
